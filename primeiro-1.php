d0217ea46d0d37c5c00ad094ada858e665cabbf2
Isabel <isabeldantas433@gmail.com>
2021-10-04 14:15:40 -0300

    alterando arquivo primeiro

---
+++ primeiro-1.php
@@ -0,0 +1,12 @@
+<?php
+
+$vnome=$_GET["nome"];
+$vend=$_GET["Endereço"];
+$vemail=$_GET["E-mail"];
+$vtel=$_GET["Telefone"];
+$vmsg=$_GET["msg"];
+
+
+echo "nome: ".$vnome. "<br/>Endereço: ".$vend."<br/>E-mail: ".$vemail."<br/>Telefone: ".$vtel."<br/>" "msg: ".$vmsg. "<br/>";
+
+?>
